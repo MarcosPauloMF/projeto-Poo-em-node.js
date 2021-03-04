ab9dd61986f6526c146b7583c3ccac93142c858b
Marcos <marcos.mpmf43@gmail.com>
2021-03-04 16:01:44 -0300

    Usando return em métodos

--- index.js
+++ index.js
@@ -6,21 +6,24 @@ class Cliente {
 class ContaCorrente {
     agencia;
     //#saldo =0
-    saldo = 0;
-
+    _saldo = 0;
+    
     sacar(valor){
-        if(this.saldo >= valor){
-            this.saldo -= valor;
+        if(this._saldo >= valor){
+            this._saldo -= valor;
         }
+        return valor;
     }
 
     depositar(valor){
-        if(valor >0 ){
-            this.saldo+= valor;
+        if(valor <=0 )
+        {
+            return;
         }
+        this._saldo+= valor;   
     }
 }
-
+//usar retorno após if para receber valor corretamente
 const cliente1 = new Cliente();
 cliente1.nome = "Marcos";
 cliente1.cpf = "1112223309";
@@ -34,9 +37,12 @@ cliente2.cpf = "3332223309";
 const ContaCorrenteMarcos = new ContaCorrente();
 ContaCorrenteMarcos.agencia = 1001;
 
+ContaCorrenteMarcos.depositar (-100);
 ContaCorrenteMarcos.depositar (100);
-ContaCorrenteMarcos.sacar(50);
+ContaCorrenteMarcos.depositar (100);
 
+const valorSacado = ContaCorrenteMarcos.sacar(50);
+console.log(valorSacado)
 
 
 console.log(ContaCorrenteMarcos);
