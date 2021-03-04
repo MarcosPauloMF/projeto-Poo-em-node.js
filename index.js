26f70dc626a5e79b9c24ab7165a54b8596a4b726
Marcos <marcos.mpmf43@gmail.com>
2021-03-04 13:16:35 -0300

    trabalhando com classes

--- index.js
+++ index.js
@@ -1,28 +1,39 @@
 class Cliente {
     nome;
     cpf;
+}
+
+class ContaCorrente {
     agencia;
     saldo;
+
+    sacar(valor){
+        if(this.saldo >= valor){
+            this.saldo -= valor;
+        }
+    }
 }
 
 const cliente1 = new Cliente();
-const cliente2 = new Cliente();
-const cliente3 = new Cliente();
-
-
 cliente1.nome = "Marcos";
 cliente1.cpf = "1112223309";
-cliente1.agencia = "1001";
-cliente1.saldo = 0;
 
+const cliente2 = new Cliente();
 cliente2.nome = "Alice";
 cliente2.cpf = "3332223309";
-cliente2.agencia = "1001";
-cliente2.saldo = 0;
 
-cliente3.nome = "Roberto";
-cliente3.cpf = "3332223309";
-cliente3.agencia = "1001";
-cliente3.saldo = 0;
+const ContaCorrenteMarcos = new ContaCorrente();
+ContaCorrenteMarcos.saldo = 0;
+ContaCorrenteMarcos.agencia = 1001;
 
-console.log(cliente1, cliente2);
+console.log(ContaCorrenteMarcos.saldo);
+ContaCorrenteMarcos.saldo = 100;
+console.log(ContaCorrenteMarcos.saldo);
+ContaCorrenteMarcos.sacar(50);
+
+
+
+
+console.log(ContaCorrenteMarcos.saldo);
+console.log(cliente1);
+console.log(cliente2);
