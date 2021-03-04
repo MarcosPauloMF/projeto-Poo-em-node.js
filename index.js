4774e45b0125321602bacc1fece177c85e7ac8a0
Marcos <marcos.mpmf43@gmail.com>
2021-03-04 17:13:53 -0300

    adicionando git ignore

--- index.js
+++ index.js
@@ -1,29 +1,6 @@
-class Cliente {
-    nome;
-    cpf;
-}
+import {Cliente} from "./Cliente.js";
+import {ContaCorrente} from "./ContaCorrente.js";
 
-class ContaCorrente {
-    agencia;
-    //#saldo =0
-    _saldo = 0;
-    
-    sacar(valor){
-        if(this._saldo >= valor){
-            this._saldo -= valor;
-        }
-        return valor;
-    }
-
-    depositar(valor){
-        if(valor <=0 )
-        {
-            return;
-        }
-        this._saldo+= valor;   
-    }
-}
-//usar retorno após if para receber valor corretamente
 const cliente1 = new Cliente();
 cliente1.nome = "Marcos";
 cliente1.cpf = "1112223309";
@@ -31,7 +8,6 @@ cliente1.cpf = "1112223309";
 const cliente2 = new Cliente();
 cliente2.nome = "Alice";
 cliente2.cpf = "3332223309";
-
 
 
 const ContaCorrenteMarcos = new ContaCorrente();
@@ -46,3 +22,9 @@ console.log(valorSacado)
 
 
 console.log(ContaCorrenteMarcos);
+
+
+
+//criar package.json
+//npm init
+//"type": "module"
