38a936878c6f7ace1bba0ad93f269dfbe5e82852
Marcos <marcos.mpmf43@gmail.com>
2021-03-09 21:54:23 -0300

    corrigindo problemas do código e adicionando componentes de heranças e proteções

--- index.js
+++ index.js
@@ -1,18 +1,17 @@
 import {Cliente} from "./Cliente.js";
 import {ContaCorrente} from "./ContaCorrente.js";
 import {ContaPoupanca } from "./ContaPoupanca.js";
-import {Conta} from "./Conta.js";
 
 const cliente1 = new Cliente("Marcos", 1112223309);
 
-const ContaCorrenteMarcos = new ContaCorrente( 0, cliente1, 1001);
+const ContaCorrenteMarcos = new ContaCorrente(cliente1, 1001);
 ContaCorrenteMarcos.depositar(500);
 ContaCorrenteMarcos.sacar(100);
 //const Conta2 = new ContaCorrente(1001, cliente1);
 
-const ContaPoupanca = new ContaCorrente( 50, cliente1, 1001);
-ContaPoupanca.sacar(10);
+const contaPoupanca = new ContaCorrente(50, cliente1, 1001);
+contaPoupanca.sacar(10);
 
 
-console.log(ContaPoupanca);
+console.log(contaPoupanca);
 console.log(ContaCorrenteMarcos);
