d3898c53702213d32e00ee8e3b345f97834c7a51
Marcos <marcos.mpmf43@gmail.com>
2021-03-11 23:06:36 -0300

    metodos abstratos e proteções

--- index.js
+++ index.js
@@ -2,11 +2,14 @@ import {Cliente} from "./Cliente.js";
 import {ContaCorrente} from "./ContaCorrente.js";
 import {ContaPoupanca } from "./ContaPoupanca.js";
 import { Conta } from "./Conta.js";
+import { ContaSalario } from "./ContaSalario.js";
 
 const cliente1 = new Cliente("Marcos", 1112223309);
 
 const ContaCorrenteMarcos = new ContaCorrente(cliente1, 1001);
 const contaPoupanca = new ContaCorrente(50, cliente1, 1001);
-const conta = new Conta(0, cliente1, 1001);
+const contaSalario = new ContaSalario (cliente1);
+contaSalario.depositar(100);
+contaSalario.sacar(10);
 
-console.log(conta);
+console.log(contaSalario);
