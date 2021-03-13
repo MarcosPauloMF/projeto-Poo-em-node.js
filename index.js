da8a03088feb46053709758f7c91cbbae9b5c0f5
Marcos <marcos.mpmf43@gmail.com>
2021-03-13 18:18:22 -0300

    trabalhando com polimosrfismo e senhas no js

--- index.js
+++ index.js
@@ -1,15 +1,14 @@
-import {Cliente} from "./Cliente.js";
-import {ContaCorrente} from "./Conta/ContaCorrente.js";
-import {ContaPoupanca } from "./Conta/ContaPoupanca.js";
-import { Conta } from "./Conta/Conta.js";
-import { ContaSalario } from "./Conta/ContaSalario.js";
+import{Cliente} from "./Cliente.js"
+import{Gerente} from "./Funcionarios/Gerente.js"
+import{Diretor} from "./Funcionarios/Diretor.js"
+import { SistemaAutenticacao } from "./SistemaAutenticacao.js";
 
-const cliente1 = new Cliente("Marcos", 1112223309);
 
-const ContaCorrenteMarcos = new ContaCorrente(cliente1, 1001);
-const contaPoupanca = new ContaCorrente(50, cliente1, 1001);
-const contaSalario = new ContaSalario (cliente1);
-contaSalario.depositar(100);
-contaSalario.sacar(10);
+const diretor = new Diretor("Rodrigo", 10000, 12345678900);
+diretor.cadastrarSenha("123456")
+const gerente = new Gerente("Marcos", 5000, 1234567654321);
+gerente.cadastrarSenha("123")
 
-console.log(contaSalario);
+const estaLogado = SistemaAutenticacao.login(diretor, "123456789");
+
+console.log(estaLogado);
