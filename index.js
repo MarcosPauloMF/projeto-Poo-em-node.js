62dc4fcbbf27c01c9c4fd8d596e63107abf1cea8
Marcos <marcos.mpmf43@gmail.com>
2021-03-14 16:46:41 -0300

    trabalhando com comportamento indefinido e opções para herança

--- index.js
+++ index.js
@@ -8,7 +8,14 @@ const diretor = new Diretor("Rodrigo", 10000, 12345678900);
 diretor.cadastrarSenha("123456")
 const gerente = new Gerente("Marcos", 5000, 1234567654321);
 gerente.cadastrarSenha("123")
+const cliente = new Cliente("Lais", 9876, "456");
 
-const estaLogado = SistemaAutenticacao.login(diretor, "123456789");
 
-console.log(estaLogado);
+const diretorEstaLogado = SistemaAutenticacao.login(diretor, "123456");
+const gerenteEstaLogado = SistemaAutenticacao.login(gerente, "123");
+const clienteEstaLogado = SistemaAutenticacao.login(cliente, "456");
+
+
+
+
+console.log(gerenteEstaLogado, diretorEstaLogado, clienteEstaLogado);
